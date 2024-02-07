d13e6e1a563f656235476b4cad44b6d07358beb2
Chhotu <chhotu@pixlerlab.com>
2024-02-07 13:02:49 +0530

    route name change

--- src/routes.js
+++ src/routes.js
@@ -16,9 +16,9 @@ router
 router
   .route("/login")
   .post(validation.loginValidation, AuthController.auth.login);
-router.route("/user/list").get(adminAuth, UserController.user.list);
+router.route("/users").get(adminAuth, UserController.user.list);
 router.route("/user/:id").get(adminAuth, UserController.user.showOne);
-router.route("/user/delete/:id").delete(adminAuth, UserController.user.delete);
-router.route("/user/update/:id").put(adminAuth, UserController.user.update);
+router.route("/user/:id").delete(adminAuth, UserController.user.delete);
+router.route("/user/:id").put(adminAuth, UserController.user.update);
 
 module.exports = router;
