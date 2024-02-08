06aabe01490d0ebccaf813d1446360b75fcda96b
Chhotu <chhotu@pixlerlab.com>
2024-02-08 13:13:04 +0530

    count by user role

---
+++ src/constrollers/DashboardController.js
@@ -0,0 +1,29 @@
+const User = require("./../models/User");
+const { response } = require("../utils/response");
+exports.dashboard = {
+  rolebasedCount: async function (req, res) {
+    try {
+      const aggregatorOpts = [
+        {
+          $group: {
+            _id: "$role",
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            roles: { $push: { role: "$_id", count: "$count" } },
+            totalUsers: { $sum: "$count" },
+          },
+        },
+      ];
+      const data = await User.aggregate(aggregatorOpts).exec();
+      return res.json(
+        response({ data: data, status: true, message: "Fetch record success" })
+      );
+    } catch (error) {
+      return res.json(response({ errors: error, message: error?.message }));
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const AuthController = require("./constrollers/AuthController");
 const UserController = require("./constrollers/UserController");
+const DashboardController = require("./constrollers/DashboardController");
 
 const validation = require("./utils/validation");
 const middleware = require("./middleware/auth");
@@ -20,5 +21,8 @@ router.route("/users").get(adminAuth, UserController.user.list);
 router.route("/user/:id").get(adminAuth, UserController.user.showOne);
 router.route("/user/:id").delete(adminAuth, UserController.user.delete);
 router.route("/user/:id").put(adminAuth, UserController.user.update);
+router
+  .route("/dashboard")
+  .get(adminAuth, DashboardController.dashboard.rolebasedCount);
 
 module.exports = router;
